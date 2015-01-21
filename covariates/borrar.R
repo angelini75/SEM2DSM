54b6b383443c7a6159676efbe8c100bc279c1c9c
Marcos <angelini75@gmail.com>
2015-01-21 21:48:10 +0100

    Listado de comandos
    comandos de consola

--- covariates/borrar.R
+++ covariates/borrar.R
@@ -1,1 +1,18 @@
 # borrar
+
+
+# 824  cd /documents
+# 825  cd /home/marcos/Documents
+# 826  git clone https://github.com/angelini75/SEM2DSM.git
+# 827  cd SEM2DSM/
+#   828  ls
+# 829  ls -la
+# 830  git status
+# 831  git add borrar.R
+# 832  git add covariates/borrar.R
+# 833  git commit
+# 834  git config --global user.email "angelini75@gmail.com"
+# 835  git commit
+# 836  git pull
+# 837  git push
+# 838  history 
