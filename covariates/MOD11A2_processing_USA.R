7ef55e0986cc31a498c1c84ffb005aa8ebc0efb0
Marcos Angelini <marcos.angelini@wur.nl>
2016-11-23 13:28:24 +0100

    Updated for Platte area

--- covariates/MOD11A2_processing_USA.R
+++ covariates/MOD11A2_processing_USA.R
@@ -40,11 +40,14 @@
 # 
 rm(list=ls())
 #setwd("/media/marcos/L0135974_DATA/UserData/BaseARG/COVARIATES/MODIS") # @RStudio desktop
-setwd("~/big/USA/MODIS/")
+setwd("~/big/USA/MODIS/") # @RStudio server
+#write("/home/mangelini/big/temp/'", file=file.path(Sys.getenv('R_SESSION_TMPDIR'), '.Renviron')) # @RStudio
+#library(raster)
+library(rgdal)
+#library(gdalUtils)
 library(doParallel)
-require(RCurl)
 
-
+# functions to read directories and files
 ##read directories from ftp site (fixed MODIS periods). Function developed by Ype.
 readdir<-function(fil){
   filenames <- try(getURL(fil,ftp.use.epsv = FALSE, dirlistonly = TRUE))
@@ -71,130 +74,53 @@ URL <- "ftp://ladsweb.nascom.nasa.gov/allData/5"
 MODISP <- "MOD11A2"
 # driver and bands of HDF file for GDAL
 driver <- "HDF4_EOS:EOS_GRID:"
-band1 <- ":MODIS_Grid_8Day_1km_LST:LST_Day_1km"
-#band2 <- ":MOD_Grid_BRDF:Nadir_Reflectance_Band5"
+bands <- data.frame(name = c(":MODIS_Grid_8Day_1km_LST:LST_Day_1km"),
+                    file.name = c("LST"))
 #define tiles
-tiles <- c("h09v05", "h10v04", "h10v05")
+tiles <- c("h10v04", "h10v05")
 #define years
 yrs <- as.character(2000:2015)
 #get periods
-period <- gsub("ftp://ladsweb.nascom.nasa.gov/allData/5/MCD43A4/2005/","",
-               readdir("ftp://ladsweb.nascom.nasa.gov/allData/5/MCD43A4/2005/"))
-# get file urls.
-u <- NULL
-for(i in seq_along(yrs)){
-    u <-append(u,readdir(paste0(URL,"/",MODISP,"/", yrs[i],"/")))
-}
-u <- paste0(u,"/")
+period <- gsub("ftp://ladsweb.nascom.nasa.gov/allData/5/MOD11A2/2005/","",
+               readdir("ftp://ladsweb.nascom.nasa.gov/allData/5/MOD11A2/2005/"))
 
-# TILE #1
-n <- NULL 
-for(i in seq_along(u)){
-  z <- readfiles(u[i])
-  n[i] <- z[grep(tiles[1], z)][1]
-  ifelse(test = is.na(n[i]),
-         n[i] <- z[grep(tiles[1], z)][1],
-         n[i])
-}
-u1 <- u[which(is.na(n))]
+# Files to be download ####
+# http://reverb.echo.nasa.gov/reverb/
+MODProd <- read.csv(file = "MOD11A2_save_results_csv.csv")
 
-# download HDF from urls
+# two dataframes, one for each tile
+h10v04 <- MODProd[grep(pattern = "?h10v04",x = MODProd$Producer.Granule.ID),]
+h10v05 <- MODProd[grep(pattern = "?h10v05",x = MODProd$Producer.Granule.ID),]
+h10v04$Online.Access.URLs <- as.character(h10v04$Online.Access.URLs)
+h10v05$Online.Access.URLs <- as.character(h10v05$Online.Access.URLs)
+
+# Download the files ####
 registerDoParallel(cores=10)
-foreach(i = seq_along(n)) %dopar%{
-  download.file(n[i], 
-                destfile = paste("output/",tiles[1],"/",
-                                 substr(x = n[i],start = 66,stop = 73),".hdf", sep=""), 
-                quiet = TRUE, mode = "wb", method = "wget")
-  }
-
-# TILE #2
-n <- NULL 
-for(i in seq_along(u)){
-  z <- readfiles(u[i])
-  n[i] <- z[grep(tiles[2], z)][1]
-  ifelse(test = is.na(n[i]),
-         n[i] <- z[grep(tiles[2], z)][1],
-         n[i])
+foreach(i = seq_along(h10v04[,1])) %dopar%{
+  download.file(h10v04[i,5], 
+                destfile = paste("output/",
+                                 tiles[1],
+                                 "/",
+                                 substr(x = h10v04[i,2],start = 9,stop = 16),
+                                 ".hdf",
+                                 sep=""),
+                quiet = TRUE, 
+                mode = "wb",
+                method = "wget")
 }
-u2 <- u[which(is.na(n))]
-
-# download HDF from urls
-registerDoParallel(cores=10)
-foreach(i = seq_along(n)) %dopar%{
-  download.file(n[i], 
-                destfile = paste("output/",tiles[2],"/",
-                                 substr(x = n[i],start = 66,stop = 73),".hdf", sep=""), 
-                quiet = TRUE, mode = "wb", method = "wget")
+foreach(i = seq_along(h10v05[,1])) %dopar%{
+  download.file(h10v05[i,5], 
+                destfile = paste("output/",
+                                 tiles[2],
+                                 "/",
+                                 substr(x = h10v05[i,2],start = 9,stop = 16),
+                                 ".hdf",
+                                 sep=""),
+                quiet = TRUE, 
+                mode = "wb",
+                method = "wget")
 }
 
-
-# TILE #3
-n <- NULL 
-for(i in seq_along(u)){
-  z <- readfiles(u[i])
-  n[i] <- z[grep(tiles[3], z)][1]
-  ifelse(test = is.na(n[i]),
-         n[i] <- z[grep(tiles[3], z)][1],
-         n[i])
-}
-u3 <- u[which(is.na(n))]
-
-# download HDF from urls
-registerDoParallel(cores=10)
-foreach(i = seq_along(n)) %dopar%{
-  download.file(n[i], 
-                destfile = paste("output/",tiles[3],"/",
-                                 substr(x = n[i],start = 66,stop = 73),".hdf", sep=""), 
-                quiet = TRUE, mode = "wb", method = "wget")
-}
-
-u1
-u2
-u3
-
-# rescuing missing files
-granules <- read.csv(file = "MOD11A2_2001-02-05-06-07-08-09-11-13-14.csv")
-miss <- c("MOD11A2.A2001353.h09v05", "MOD11A2.A2002001.h09v05", "MOD11A2.A2002017.h09v05",
-          "MOD11A2.A2002145.h09v05", "MOD11A2.A2002153.h09v05", "MOD11A2.A2005353.h09v05",
-          "MOD11A2.A2006033.h09v05", "MOD11A2.A2009289.h09v05", "MOD11A2.A2009321.h09v05",
-          "MOD11A2.A2001225.h10v04", "MOD11A2.A2001233.h10v04", "MOD11A2.A2001249.h10v04",
-          "MOD11A2.A2001281.h10v04", "MOD11A2.A2001289.h10v04", "MOD11A2.A2001345.h10v04",
-          "MOD11A2.A2005017.h10v04", "MOD11A2.A2005025.h10v04", "MOD11A2.A2008281.h10v04",
-          "MOD11A2.A2009337.h10v04", "MOD11A2.A2011193.h10v04", "MOD11A2.A2011225.h10v04",
-          "MOD11A2.A2011233.h10v04", "MOD11A2.A2014249.h10v04", 
-          "MOD11A2.A2007281.h10v05", "MOD11A2.A2007289.h10v05", "MOD11A2.A2013233.h10v05",
-          "MOD11A2.A2013353.h10v05")
-
-# "MOD11A2.A2001177.h10v05" cannot be rescued so A2001177 is deleted from all tiles
-
-granules$Online.Access.URLs <- as.character(granules$Online.Access.URLs)
-rescue <- NULL
-for(i in seq_along(miss)){
-   rescue[i] <- 
-      unique(
-         as.character(
-            granules[
-               which(
-                  grepl(
-                     pattern = miss[i],
-                     x = granules$Producer.Granule.ID) == 1),
-               "Online.Access.URLs"
-               ]
-         )
-      )
-}
-registerDoParallel(cores=10)
-foreach(i = seq_along(rescue)) %dopar%{
-  download.file(
-    rescue[i],
-    destfile = paste(
-      "output/",
-      substr(x = rescue[i],start = 82,stop = 96),".hdf", sep=""), 
-                quiet = TRUE, mode = "wb", method = "wget")
-}
-# I changed missing file names and directory by hand afetr downloading.
-
-# files have same names at each tile
 ############### #### ### ## # - IMAGE PROCESSING - # ## ### #### ###############
 setwd("~/big/USA/MODIS")
 library(rgdal)
@@ -204,123 +130,128 @@ library(doParallel)
 library(maptools)
 ############ Create mosaic 
 # subset extension in MODIS coordinate system (could be calculated from a shape file)
-extent(readShapePoints("K_3_MODproj.shp"))
+aoi <- readShapePoly("Platte_area.shp")
 
-mask.ext <- c(-8995835.65,4052136.73, -7932725.14,4469948.27) # xmin,ymin , xmax,ymax @QGIS K_3MODproj.shp
+# define projections
+wgs84 <- CRS("+init=epsg:4326")
+modis <- CRS("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
+
+# assign projection
+proj4string(aoi) <- wgs84
+
+# use spTransform
+aoi <- spTransform(aoi, modis)
 
 
-f <-list.files("output/h09v05/", pattern = ".hdf$")
+
+f <-list.files("output/h10v04/", pattern = ".hdf$")
+# f04 <- list.files("output/h10v04/", pattern = ".hdf$")
+# f[which(f %in% f04 == F)]
 #pj <- projection(hdfImage[[1]])
+rasterOptions(tmpdir="~/big/USA/MODIS/output/temp/")
 
-registerDoParallel(cores=10)
+registerDoParallel(cores=11)
 foreach(i = seq_along(f)) %dopar%{
-  hdfImage <- list()
-  for(j in seq_along(tiles)){
-    hdfImage[[j]] <- readGDAL(paste0(driver,
-                                     "output/",
-                                     tiles[j],
-                                     "/",
-                                     f[i],
-                                     band1))  
+  for(k in seq_along(bands[,1])){
+    hdfImage <- list()
+    for(j in seq_along(tiles)){
+      hdfImage[[j]] <- readGDAL(
+        paste0("HDF4_EOS:EOS_GRID:",
+               "output/",
+               tiles[j],
+               "/",
+               f[i],
+               bands[k,1]
+        )
+      )  
+    }
+    m <- mosaic(
+      raster(hdfImage[[1]]),
+      raster(hdfImage[[2]]),
+      fun = mean)
+    m <- crop(m, aoi)
+    writeRaster(x = m, 
+                filename = paste0("output/bands/",
+                                  f[i],
+                                  ".",
+                                  bands[k,2],
+                                  ".tif"),
+                overwrite=TRUE)
+    rm(m)
+    rm(hdfImage)
   }
-  m <- mosaic(
-     raster(hdfImage[[1]]),
-     raster(hdfImage[[2]]),
-     raster(hdfImage[[3]]),
-     fun = mean
-     )
-  m <- crop(m, mask.ext)
-  writeRaster(x = m, filename = paste0("output/bands/",f[i],".LST.tif"), overwrite=TRUE)
 }
+#removeTmpFiles(h=24)
+showTmpFiles()
 
 ### Estimation of mean and sd by period
-
 # crate list of images from the same period
 list.per <- list()
 for(i in seq_along(period)){
-  list.per[[i]] <- 
-    list.files(
-      path = "output/bands/",
-      pattern= paste0(
-        period[i],
-        ".hdf.LST.tif"
-        )
-    )
+  list.per[[i]] <- list.files(path = "output/bands/",
+                              pattern= paste0(period[i],
+                                              ".hdf.LST.tif"))
 }
 
 # stack images from the same period (raster package)
 # I found this script here http://markmail.org/thread/lsjnczi5fppddrpr
-registerDoParallel(cores=12)
-foreach(
-  i = seq_along(
-    period
-  )
-) %dopar% {
-  s <- 
-    stack(
-      paste0(
-        "output/bands/",
-        list.per[[i]]
-      )
-    )
- # s <- s/10000 # to reduce no significan digits
-  # mean and sd  
-  m <- 
-    mean(
-      s,
-      na.rm = TRUE
-    )
-  std <- 
-    calc(
-      s,
-      fun = sd,
-      na.rm=TRUE
-    )
-  # save and delete objects
-  writeRaster(
-    x = m,
-    filename =  
-      paste0(
-        "output/LST/lst.",
-        period[i],".mean.tif"
-      ),
-    overwrite=T
-  )
+registerDoParallel(cores=11)
+foreach(i = seq_along(period)) %dopar% {
+  s <- stack(paste0("output/bands/",
+                    list.per[[i]]))
+  # remove images with too many NA's
+  a <- summary(s)
+  a <- as.data.frame(t(a))
+  a$name <- rownames(a)
+  s <- s[[!(which(names(s) %in% a$name[a$`NA's`>30000]))]]
   
-  writeRaster(
-    x = std,
-    filename =  
-      paste0(
-        "output/LST/lst.",
-        period[i],".sd.tif"
-      ),
-    overwrite = TRUE
-  )
+  # s <- s/10000 # to reduce no significan digits
+  # mean and sd  
+  m <- mean(s, na.rm = TRUE)
+  std <- calc(s, fun = sd, na.rm=TRUE)
+  
+  # save and delete objects
+  writeRaster(x = m,
+              filename = paste0("output/LST/LST.",
+                                period[i],
+                                ".mean.tif"),
+              overwrite=T)
+  
+  writeRaster(x = std,
+              filename =  paste0("output/LST/LST.",
+                                 period[i],
+                                 ".sd.tif"),
+              overwrite = TRUE)
   
   rm(s)
   rm(m)
   rm(std)
 }
 
-# get lstm (same as paper 2)
-library(raster)
-rm(list=ls())
-setwd("~/big/USA/MODIS/")
-f <-list.files("output/LST/", pattern = "mean.tif$")
-f
+# crate list of images from entire period
+LST <- list.files(path = "output/bands/",
+                  pattern= ".hdf.LST.tif")
 
-writeRaster(
-  x = mean(
-    stack(
-      paste0(
-        "output/LST/",
-        f
-      )
-    ),
-    na.rm = TRUE
-  ),
-  filename = 
-    "output/LST/lstm.tif",
-  overwrite = TRUE
-)
+s <- stack(paste0("output/bands/", LST))
+
+# remove images with more than 30000 NA's (10% of the image)
+a <- summary(s)
+a <- as.data.frame(t(a))
+a$name <- rownames(a)
+s <- s[[!(which(names(s) %in% a$name[a$`NA's`>30000]))]]
+# mean and sd  
+m <- mean(s, na.rm = TRUE)
+std <- calc(s, fun = sd, na.rm=TRUE)
+
+# save and delete objects
+writeRaster(x = m, filename = "output/LST/LST.mean.tif", overwrite=TRUE)
+
+writeRaster(x = std, filename = "output/LST/LST.sd.tif", overwrite = TRUE)
+
+
+a <- summary(s)
+a <- as.data.frame(t(a))
+a$name <- rownames(a)
+s1 <- s[[!(which(names(s) %in% a$name[a$`NA's`>30000]))]]
+
 
