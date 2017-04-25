8b87d126262cfe37e87d36d048000446cb291b93
Angelini, Marcos <marcos.angelini@wur.nl>
2017-04-25 10:24:00 +0200

    Compare DEoptim with nlminb

--- Paper_4/CFA_lavaan.R
+++ Paper_4/CFA_lavaan.R
@@ -43,14 +43,14 @@
 
 
 
-library(lavaan)
-HS.model <- ' visual  =~ x1 + x2 + x3
-textual =~ x4 + x5 + x6
-speed   =~ x7 + x8 + x9 
-textual ~ speed + visual
-speed ~ visual'
-
-fit <- sem(HS.model, data=HolzingerSwineford1939, estimator = "ML") # or ML
+# library(lavaan)
+# HS.model <- ' visual  =~ x1 + x2 + x3
+# textual =~ x4 + x5 + x6
+# speed   =~ x7 + x8 + x9 
+# textual ~ speed + visual
+# speed ~ visual'
+# 
+# fit <- sem(HS.model, data=HolzingerSwineford1939, estimator = "ML") # or ML
 
 fit <- my.fit.lv.ML
 # initial list of model matrices
@@ -148,18 +148,76 @@ start.x <- parTable(fit)$start[ parTable(fit)$free > 0 ]
 out.ML  <- nlminb(start = start.x, objective = objective_ML, 
                   MLIST = MLIST, control = list(eval.max = 5000, trace = 1))
 
+x2MLIST(out.ML$par, MLIST)
+
 # chi-square
-out.ML$objective * 156
+out.ML$objective * 153
 # 85.30551 
-library(DEoptim)
-results <- DEoptim(fn = objective_ML, lower = start.x-1, upper = start.x+1, 
-                   control = list(reltol=10E-15, steptol=50, itermax = 5000, trace = T, 
-                                  CR = 0.5, NP = 1000, F=0.8, strategy =2), MLIST = MLIST)
+# library(DEoptim)
+# results <- DEoptim(fn = objective_ML, lower = start.x-1, upper = start.x+1, 
+#                    control = list(reltol=10E-15, steptol=50, itermax = 5000, trace = T, 
+#                                   CR = 0.5, NP = 1000, F=0.8, strategy =2), MLIST = MLIST)
 # chi-square
-results$optim$bestval * 156
+# results$optim$bestval * 153
 
 summary(out.ML$par - results$optim$bestmem)
+
+diff <- data.frame(par=1:51, diff=out.ML$par - results$optim$bestmem)
+
 
 # > summary(out.ML$par - results$optim$bestmem)
 # Min.    1st Qu.     Median       Mean    3rd Qu.       Max. 
 # -0.0175500 -0.0011810  0.0007089  0.0173700  0.0026310  0.4331000 
+
+# par          diff
+# par1    1  3.729837e-01
+# par2    2 -1.754533e-02
+# par3    3  4.331289e-01
+# par4    4  8.171361e-02
+# par5    5  5.895941e-04
+# par6    6  1.160049e-03
+# par7    7  1.957513e-03
+# par8    8 -1.012679e-04
+# par9    9 -5.287590e-03
+# par10  10  2.166860e-03
+# par11  11  1.149968e-03
+# par12  12 -1.706141e-04
+# par13  13  8.219489e-04
+# par14  14 -3.769180e-04
+# par15  15  4.117405e-03
+# par16  16  1.118815e-03
+# par17  17  2.083414e-03
+# par18  18 -5.142453e-04
+# par19  19 -1.294106e-03
+# par20  20  1.916619e-03
+# par21  21 -5.822167e-03
+# par22  22  2.610429e-03
+# par23  23  8.023352e-03
+# par24  24 -2.596190e-04
+# par25  25 -1.067525e-03
+# par26  26  5.330471e-03
+# par27  27  6.123758e-05
+# par28  28  9.475898e-03
+# par29  29  5.810359e-03
+# par30  30  5.840557e-03
+# par31  31  7.789799e-03
+# par32  32 -7.485641e-03
+# par33  33 -3.803355e-03
+# par34  34  3.289150e-04
+# par35  35  7.055161e-04
+# par36  36 -5.983437e-03
+# par37  37  7.088888e-04
+# par38  38  2.651615e-03
+# par39  39  2.004383e-03
+# par40  40  7.173832e-04
+# par41  41  4.783426e-03
+# par42  42 -2.964310e-05
+# par43  43  4.021224e-03
+# par44  44 -7.375841e-03
+# par45  45 -1.109567e-02
+# par46  46  1.028914e-03
+# par47  47  5.574870e-04
+# par48  48 -1.659101e-03
+# par49  49 -2.847471e-03
+# par50  50 -3.047117e-03
+# par51  51 -5.870095e-03
