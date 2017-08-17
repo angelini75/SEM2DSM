151f83219f0388e9da7ae15c9cc8aac085668217
Angelini, Marcos <marcos.angelini@wur.nl>
2017-08-17 16:01:51 +0200

    Paper 4. Bootstrapping is working fine. Check outliers in ks dataset.
    Former-commit-id: 41dcd151fc1d40fb267b409690e2cd42a2b23878

--- Paper_4/lavaan_se_vs_bootstraping_estimation.R
+++ Paper_4/lavaan_se_vs_bootstraping_estimation.R
@@ -2,30 +2,40 @@
 rm(list=ls()[])
 library(lavaan)
 library(doParallel)
+library(reshape)
+library(ggplot2)
+
 load("~/Documents/SEM2DSM1/Paper_4/data/se_vs_bootstraping.RData")
+s <- ks[!(ks$OC.B >4),]
 
 # get the estimates  
-registerDoParallel(cores = 2)
+registerDoParallel(cores = 3)
 trials = 1000
 x <- foreach(icount(trials), .combine=rbind) %dopar% {
-  samples <- ks[sample(x = 1:nrow(ks), size = nrow(ks), replace = TRUE),]
-  rownames(samples) <- 1:153
+  samples <- s[sample(x = 1:nrow(s), size = nrow(s), replace = TRUE),]
+  rownames(samples) <- 1:151
   fit <- sem(model = my.model.lv,data = samples, meanstructure = FALSE, 
              fixed.x = T)
   lavaan:::lav_model_get_parameters(lavmodel = fit@Model)
 }
+x <- as.data.frame(x)
+# estimates of the model (fit) published in paper 3
+my.fit <- sem(model = my.model.lv,data = ks, meanstructure = FALSE, 
+              fixed.x = T)
+
 # names of the estimates
+p <- partable(my.fit)
 colnames(x) <- paste0(p$lhs[p$free!=0],p$op[p$free!=0],p$rhs[p$free!=0])
 # 
+
 # statistics of the estimates
 xmean <- colMeans(x)
 xsd <- apply(x, 2, sd)
-# estimates of the model (fit) published in paper 3
-my.fit <- sem(model = my.model.lv,data = ks, meanstructure = FALSE, 
-           fixed.x = T)
-my.x <- lavaan:::lav_model_get_parameters(lavmodel = my.fit@Model)
+
 xmean <- reshape::melt(as.data.frame(xmean))
 xmean$variable <- names(x)
+
+my.x <- lavaan:::lav_model_get_parameters(lavmodel = my.fit@Model)
 my.x <- reshape::melt(as.data.frame(my.x))
 my.x$variable <- names(x)
 
@@ -34,6 +44,11 @@ ggplot2::ggplot(data = reshape::melt(x), mapping = aes(x = value)) +
   geom_histogram(bins = 40) + facet_wrap(~variable, scales = 'free_x') +
   geom_vline(aes(xintercept=value, color='my.x'), data = my.x) +
   geom_vline(aes(xintercept=value, color='bootstrap'), data=xmean)
+
+p <- partable(my.fit)
+p$se.boots <- NA
+p$se.boots[p$free != 0] <- xsd
+p$se[p$free != 0] - p$se.boots[p$free != 0]
 
 lav_model_x2GLIST <- function (lavmodel = NULL, x = NULL) 
 {
