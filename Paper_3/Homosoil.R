0775df8ab8e8b898208635368b4bc481685a45fe
angelini75 <angelini75@gmail.com>
2018-04-18 13:57:55 -0300

    Paper 3. modification homosoil function. Individual homologous maps.

--- Paper_3/Homosoil.R
+++ Paper_3/Homosoil.R
@@ -115,6 +115,257 @@ levelplot(result[[1]], col.regions = area_colors,
   layer(sp.points(ba, col = "green", pch = 20, cex = 2))+ 
   layer(sp.points(ks, col = "green", pch = 20, cex = 2))
 
+# modification homosoil function
 
-
+homotop <- function (grid_data,recipient.lon,recipient.lat) {
+  #Gower’s similarity function
+  gower <- function(c1, c2, r) 1-(abs(c1-c2)/r)
+  #index global data
+  grid.lon <-grid_data[,1] #longitude
+  grid.lat <-grid_data[,2] #latitude
+  grid.climate <-grid_data[,3:54] #climate data
+  grid.lith <-grid_data[,58] #lithology
+  grid.topo <-grid_data[,55:57] #topography
+  #data frame to put outputs of homosoil function
+  world.grid<- data.frame(grid_data[, c("X", "Y")],
+                          fid = seq(1, nrow(grid_data), 1), homologue = 0, homoclim=0,
+                          homolith=0, homotop=0)
+  # find the closest recipient point
+  dist = sqrt((recipient.lat - grid.lat)^2 + (recipient.lon
+                                              - grid.lon)^2)
+  imin = which.min(dist)
   
+  # climate, lithology and topography for recipient site
+  recipient.climate <- grid.climate[imin, ]
+  recipient.lith <- grid.lith[imin]
+  recipient.topo <- grid.topo[imin, ]
+  
+  # range of climate variables
+  rv <- apply(grid.climate, 2, range)
+  rr <- rv[2, ] - rv[1, ]
+  # calculate similarity to all variables in the grid
+  S <- (mapply(gower, c1 = grid.climate, c2 = recipient.climate,
+               r = rr))
+  Sr <- apply(S, 1, mean)# take the average
+  
+  # row index for homoclime with top X% similarity.
+  iclim = which(Sr >= quantile(Sr, 0.85), arr.ind = TRUE)
+  # save homoclime result
+  world.grid$homologue[iclim] <- 1
+  world.grid$homoclim[iclim] <- 1
+  
+  # find within homoclime, areas with homolith
+  ilith = which(grid.lith == recipient.lith, arr.ind = TRUE)
+  #global comparison
+  # homolith in areas of homoclime
+  clim.match <- which(world.grid$homologue == 1)
+  climlith.match <- clim.match[clim.match %in% ilith]
+  
+  # save homolith result
+  world.grid$homologue[climlith.match] <- 2
+  world.grid$homolith[climlith.match] <- 1
+  
+  # range of topographic variables
+  rv <- apply(grid.topo, 2, range)
+  rt <- rv[2, ] - rv[1, ]
+  # calculate similarity of topographic variables
+  Sa <- (mapply(gower, c1 = grid.topo, c2 = recipient.topo, r = rt))
+  St <- apply(Sa, 1, mean) # take the average
+  # row index for homotop
+  itopo = which(St >= quantile(St, 0.85), arr.ind = TRUE)
+  
+  top.match <- which(world.grid$homologue == 2)
+  lithtop.match <- top.match[top.match %in% itopo]
+  # save homotop result
+  world.grid$homologue[lithtop.match] <- 3
+  world.grid$homotop[lithtop.match] <- 1
+  
+  # homologue raster object
+  r1 <- rasterFromXYZ(world.grid[, c(1, 2, 7)])
+  r1 <- as.factor(r1)
+  rat <- levels(r1)[[1]]
+  rat[["homologue"]] <- c("", "homotop")
+  levels(r1) <- rat
+  
+  retval <- list(r1, world.grid)
+  return(retval)}
+
+homolith <- function (grid_data,recipient.lon,recipient.lat) {
+  #Gower’s similarity function
+  gower <- function(c1, c2, r) 1-(abs(c1-c2)/r)
+  #index global data
+  grid.lon <-grid_data[,1] #longitude
+  grid.lat <-grid_data[,2] #latitude
+  grid.climate <-grid_data[,3:54] #climate data
+  grid.lith <-grid_data[,58] #lithology
+  grid.topo <-grid_data[,55:57] #topography
+  #data frame to put outputs of homosoil function
+  world.grid<- data.frame(grid_data[, c("X", "Y")],
+                          fid = seq(1, nrow(grid_data), 1), homologue = 0, homoclim=0,
+                          homolith=0, homotop=0)
+  # find the closest recipient point
+  dist = sqrt((recipient.lat - grid.lat)^2 + (recipient.lon
+                                              - grid.lon)^2)
+  imin = which.min(dist)
+  
+  # climate, lithology and topography for recipient site
+  recipient.climate <- grid.climate[imin, ]
+  recipient.lith <- grid.lith[imin]
+  recipient.topo <- grid.topo[imin, ]
+  
+  # range of climate variables
+  rv <- apply(grid.climate, 2, range)
+  rr <- rv[2, ] - rv[1, ]
+  # calculate similarity to all variables in the grid
+  S <- (mapply(gower, c1 = grid.climate, c2 = recipient.climate,
+               r = rr))
+  Sr <- apply(S, 1, mean)# take the average
+  
+  # row index for homoclime with top X% similarity.
+  iclim = which(Sr >= quantile(Sr, 0.85), arr.ind = TRUE)
+  # save homoclime result
+  world.grid$homologue[iclim] <- 1
+  world.grid$homoclim[iclim] <- 1
+  
+  # find within homoclime, areas with homolith
+  ilith = which(grid.lith == recipient.lith, arr.ind = TRUE)
+  #global comparison
+  # homolith in areas of homoclime
+  clim.match <- which(world.grid$homologue == 1)
+  climlith.match <- clim.match[clim.match %in% ilith]
+  
+  # save homolith result
+  world.grid$homologue[climlith.match] <- 2
+  world.grid$homolith[climlith.match] <- 1
+  
+  # range of topographic variables
+  rv <- apply(grid.topo, 2, range)
+  rt <- rv[2, ] - rv[1, ]
+  # calculate similarity of topographic variables
+  Sa <- (mapply(gower, c1 = grid.topo, c2 = recipient.topo, r = rt))
+  St <- apply(Sa, 1, mean) # take the average
+  # row index for homotop
+  itopo = which(St >= quantile(St, 0.85), arr.ind = TRUE)
+  
+  top.match <- which(world.grid$homologue == 2)
+  lithtop.match <- top.match[top.match %in% itopo]
+  # save homotop result
+  world.grid$homologue[lithtop.match] <- 3
+  world.grid$homotop[lithtop.match] <- 1
+  
+  # homologue raster object
+  r1 <- rasterFromXYZ(world.grid[, c(1, 2, 6)])
+  r1 <- as.factor(r1)
+  rat <- levels(r1)[[1]]
+  rat[["homologue"]] <- c("", "homolith")
+  levels(r1) <- rat
+  
+  retval <- list(r1, world.grid)
+  return(retval)}
+
+homoclime <- function (grid_data,recipient.lon,recipient.lat) {
+  #Gower’s similarity function
+  gower <- function(c1, c2, r) 1-(abs(c1-c2)/r)
+  #index global data
+  grid.lon <-grid_data[,1] #longitude
+  grid.lat <-grid_data[,2] #latitude
+  grid.climate <-grid_data[,3:54] #climate data
+  grid.lith <-grid_data[,58] #lithology
+  grid.topo <-grid_data[,55:57] #topography
+  #data frame to put outputs of homosoil function
+  world.grid<- data.frame(grid_data[, c("X", "Y")],
+                          fid = seq(1, nrow(grid_data), 1), homologue = 0, homoclim=0,
+                          homolith=0, homotop=0)
+  # find the closest recipient point
+  dist = sqrt((recipient.lat - grid.lat)^2 + (recipient.lon
+                                              - grid.lon)^2)
+  imin = which.min(dist)
+  
+  # climate, lithology and topography for recipient site
+  recipient.climate <- grid.climate[imin, ]
+  recipient.lith <- grid.lith[imin]
+  recipient.topo <- grid.topo[imin, ]
+  
+  # range of climate variables
+  rv <- apply(grid.climate, 2, range)
+  rr <- rv[2, ] - rv[1, ]
+  # calculate similarity to all variables in the grid
+  S <- (mapply(gower, c1 = grid.climate, c2 = recipient.climate,
+               r = rr))
+  Sr <- apply(S, 1, mean)# take the average
+  
+  # row index for homoclime with top X% similarity.
+  iclim = which(Sr >= quantile(Sr, 0.85), arr.ind = TRUE)
+  # save homoclime result
+  world.grid$homologue[iclim] <- 1
+  world.grid$homoclim[iclim] <- 1
+  
+  # find within homoclime, areas with homolith
+  ilith = which(grid.lith == recipient.lith, arr.ind = TRUE)
+  #global comparison
+  # homolith in areas of homoclime
+  clim.match <- which(world.grid$homologue == 1)
+  climlith.match <- clim.match[clim.match %in% ilith]
+  
+  # save homolith result
+  world.grid$homologue[climlith.match] <- 2
+  world.grid$homolith[climlith.match] <- 1
+  
+  # range of topographic variables
+  rv <- apply(grid.topo, 2, range)
+  rt <- rv[2, ] - rv[1, ]
+  # calculate similarity of topographic variables
+  Sa <- (mapply(gower, c1 = grid.topo, c2 = recipient.topo, r = rt))
+  St <- apply(Sa, 1, mean) # take the average
+  # row index for homotop
+  itopo = which(St >= quantile(St, 0.85), arr.ind = TRUE)
+  
+  top.match <- which(world.grid$homologue == 2)
+  lithtop.match <- top.match[top.match %in% itopo]
+  # save homotop result
+  world.grid$homologue[lithtop.match] <- 3
+  world.grid$homotop[lithtop.match] <- 1
+  
+  # homologue raster object
+  r1 <- rasterFromXYZ(world.grid[, c(1, 2, 5)])
+  r1 <- as.factor(r1)
+  rat <- levels(r1)[[1]]
+  rat[["homologue"]] <- c("", "homoclime")
+  levels(r1) <- rat
+  
+  retval <- list(r1, world.grid)
+  return(retval)}
+
+
+top <- homotop(grid_data = homosoil_globeDat,
+                   recipient.lon = recipient.lon,
+                   recipient.lat = recipient.lat)
+
+
+# plot
+area_colors <- c("#EFEFEF", "#666666")
+levelplot(top[[1]], col.regions = area_colors,
+          xlab = "", ylab = "") + 
+  layer(sp.points(ba, col = "green", pch = 20, cex = 2))+ 
+  layer(sp.points(ks, col = "green", pch = 20, cex = 2))
+
+
+clime <- homoclime(grid_data = homosoil_globeDat,
+               recipient.lon = recipient.lon,
+               recipient.lat = recipient.lat)
+# plot
+area_colors <- c("#EFEFEF", "#666666")
+levelplot(clime[[1]], col.regions = area_colors,
+          xlab = "", ylab = "") + 
+  layer(sp.points(ba, col = "green", pch = 20, cex = 2))+ 
+  layer(sp.points(ks, col = "green", pch = 20, cex = 2))
+
+lith <- homolith(grid_data = homosoil_globeDat,
+                   recipient.lon = recipient.lon,
+                   recipient.lat = recipient.lat)
+# plot
+area_colors <- c("#EFEFEF", "#666666")
+levelplot(lith[[1]], col.regions = area_colors,
+          xlab = "", ylab = "") + 
+  layer(sp.points(ba, col = "green", pch = 20, cex = 2))+ 
+  layer(sp.points(ks, col = "green", pch = 20, cex = 2))
